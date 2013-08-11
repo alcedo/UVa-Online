e29e2d14e7ffb02defc71beed79fa6abc05bf3be
alcedo <konsolidate@gmail.com>
2013-08-12 00:21:18 +0800

    Added basic files for problemset

---
+++ src/com/easy/Problem_11498.java
@@ -0,0 +1,16 @@
+package com.easy;
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Problem_11498 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int lines = in.nextInt();
+
+        while(0 != lines--) {
+      
+        }
+    }
+}
