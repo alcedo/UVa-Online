a2bdb9804af088196e98884865a0e7657faaa14a
victor <konsolidate@gmail.com>
2013-08-11 23:28:23 +0800

    basic files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Victor
+ * Date: 11/8/13
+ * Time: 11:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
