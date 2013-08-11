3b37d1c6da0d69b01352e371b3e3c54da1f5bc34
alcedo <konsolidate@gmail.com>
2013-08-12 00:32:08 +0800

    Update Problem_11172.java

--- src/com/easy/Problem_11172.java
+++ src/com/easy/Problem_11172.java
@@ -3,6 +3,8 @@ package com.easy;
 import java.util.Scanner;
 import static java.lang.System.*;
 
+// Simple problem to test out UVA submission system. 
+// *A one-liner could be written for this. lhs > rhs ? print > : print <; 
 public class Problem_11172 {
 
     public static void main(String[] args) {
