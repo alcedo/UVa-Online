9725f9faa8f4b6f85ee20fcc11768c799856aced
victor <konsolidate@gmail.com>
2013-08-17 14:16:07 +0800

    added  Problem_11507

---
+++ src/com/easy/Problem_11507.java
@@ -0,0 +1,13 @@
+package com.easy;
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+// Simple problem to test out UVA submission system. 
+// *A one-liner could be written for this. lhs > rhs ? print > : print <; 
+public class Problem_11507 {
+
+    public static void main(String[] args) {
+      
+    }
+}
