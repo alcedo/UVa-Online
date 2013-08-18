bf1a6d2955e0a225d4eb89a597d21b63461034ca
victor <konsolidate@gmail.com>
2013-08-18 12:36:10 +0800

    finished problem 10114

--- src/com/easy/Problem_10114.java
+++ src/com/easy/Problem_10114.java
@@ -5,17 +5,16 @@ import static java.lang.System.*;
 
 /**
  * mistake: forgot to reset hashmap ! generally quite forgetful about resetting
- * 'holders'
+ * flags and statuses. Derived values are still fine.
  * 
  * Don't use antbuild to view results! '\n' characters are not shown
  * 
+ * There's no *saved* solution for this because i stupidly used git branch and git checkout without
+ * committing and saving those changes first :( 
  * @author Victor
  */
 public class Problem_10114 {
 
     public static void main(String[] args) {
-    	//This solution is EMPTY because i stupidly used git branch and git checkout without
-    	//Commiting / Savings changes first!!!
-    	//Lesson learnt :(
     }
 }
