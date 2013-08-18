2cf8052ea112542fff0141684ac687597a312149
victor <konsolidate@gmail.com>
2013-08-18 12:32:38 +0800

    Problem 10114 Finished

---
+++ src/com/easy/Problem_10114.java
@@ -0,0 +1,21 @@
+package com.easy;
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+/**
+ * mistake: forgot to reset hashmap ! generally quite forgetful about resetting
+ * 'holders'
+ * 
+ * Don't use antbuild to view results! '\n' characters are not shown
+ * 
+ * @author Victor
+ */
+public class Problem_10114 {
+
+    public static void main(String[] args) {
+    	//This solution is EMPTY because i stupidly used git branch and git checkout without
+    	//Commiting / Savings changes first!!!
+    	//Lesson learnt :(
+    }
+}
