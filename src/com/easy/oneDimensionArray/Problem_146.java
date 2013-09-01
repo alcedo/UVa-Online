9a98d6636e6fe09965e977bbe9e43d9dabba3f81
victor <konsolidate@gmail.com>
2013-09-01 17:04:05 +0800

    finished Problem_146

---
+++ src/com/easy/oneDimensionArray/Problem_146.java
@@ -0,0 +1,304 @@
+package com.easy.oneDimensionArray;
+
+public class Problem_146 {
+
+}
+
+
+
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import static java.lang.System.out;
+
+/**
+ * Use this file to submit. UVA only accepts class Main
+ */
+public class Main {
+	public static final boolean DEBUG = false;
+	
+    public static void main(String[] args) throws IOException {
+    	 
+		BufferedOutputStream output = new BufferedOutputStream(System.out);
+		InputReader input           = new InputReader(System.in);
+		StringBuilder sb = new StringBuilder(2000); //assume initial size of 2000 chars
+		  
+		String str;  
+		while((str = input.readLine()) != null) {
+			if(str.compareTo("#") == 0) break;
+			//StringTokenizer stringTokenizer = new StringTokenizer(str);
+			//int num = Integer.parseInt(stringTokenizer.nextToken());
+			
+			char[] array = str.toCharArray();
+			Character[] code = new Character[array.length];
+			for(int i=0; i<array.length; i++){
+				code[i] = new Character(array[i]);
+			}
+			
+			Character[] newPmt = (Character[])nextPermutation(code);
+			
+			if(newPmt == null)
+				sb.append("No Successor" + "\n");
+			else
+			{
+				for(int i=0; i<code.length; i++)
+					sb.append(code[i]);
+				
+				sb.append("\n");
+			}
+				
+		}
+		
+		output.write(sb.toString().getBytes());
+		output.flush();
+		output.close();
+    
+    }// end of static void main
+
+
+	/** Permutations **/ 
+	// simply prints all permutation - to see how it works
+	private static void printPermutations( Comparable[] c ) {
+		System.out.println( Arrays.toString( c ) );
+		while ( ( c = nextPermutation( c ) ) != null ) {
+			System.out.println( Arrays.toString( c ) );
+		}
+	}
+
+	// modifies c to next permutation or returns null if such permutation does not exist
+	private static Comparable[] nextPermutation( final Comparable[] c ) {
+		// 1. finds the largest k, that c[k] < c[k+1]
+		int first = getFirst( c );
+		if ( first == -1 ) return null; // no greater permutation
+		// 2. find last index toSwap, that c[k] < c[toSwap]
+		int toSwap = c.length - 1;
+		while ( c[ first ].compareTo( c[ toSwap ] ) >= 0 )
+			--toSwap;
+		// 3. swap elements with indexes first and last
+		swap( c, first++, toSwap );
+		// 4. reverse sequence from k+1 to n (inclusive) 
+		toSwap = c.length - 1;
+		while ( first < toSwap )
+			swap( c, first++, toSwap-- );
+		return c;
+	}
+
+	// finds the largest k, that c[k] < c[k+1]
+	// if no such k exists (there is not greater permutation), return -1
+	private static int getFirst( final Comparable[] c ) {
+		for ( int i = c.length - 2; i >= 0; --i )
+			if ( c[ i ].compareTo( c[ i + 1 ] ) < 0 )
+				return i;
+		return -1;
+	}
+
+	// swaps two elements (with indexes i and j) in array 
+	private static void swap( final Comparable[] c, final int i, final int j ) {
+		final Comparable tmp = c[ i ];
+		c[ i ] = c[ j ];
+		c[ j ] = tmp;
+	}
+	
+
+	public static class Character implements Comparable<Character> { 
+		public char c; 
+		
+		public Character(char c) {this.c = c;}
+		
+		public int compareTo(Character charCompare) {
+			return this.c - charCompare.c;
+		}	
+		
+		public String toString(){
+			return ""+this.c;
+		}
+	}
+
+
+	    
+}//End of Main Class
+
+
+
+
+
+/**************************************************************************
+ *  Fast I/O functions
+ **************************************************************************/
+//BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+/* 
+Slightly less efficient 
+BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out)); 
+output.write(stringBuilder.toString());
+
+More efficient: 
+BufferedOutputStream output = new BufferedOutputStream(System.out); // more efficient  
+output.write(stringBuilder.toString().getBytes());
+*/
+
+//or we can use the below utilitiy classes: 
+
+/** 
+* Various utility class for Fast I/O in java.  
+* Readings: http://www.codechef.com/wiki/java#IO_in_Java 
+* BufferedInputStream > BufferedReader > Scanner  
+* 
+* USAGE: 
+* InputReader in 		= new InputReader(System.in);
+* OutputWriter out	=	new OutputWriter(System.out);
+* 
+* int i = in.readInt(); //read int
+* String s = in.readString(); //read string
+* int[] x = IOUtils.readIntArray(in,N); //read int array of size N
+* String s = in.readLine() // read a line ( to be used with tokenizer );
+* 
+* out.printLine("X");
+*  
+* out.flush(); // flush output
+* out.close(); // remember to close the outputstream, at the end (might be able to not do this to save some time) 
+*/
+
+class InputReader {
+
+	private InputStream stream;
+	private byte[] buf = new byte[1024];
+	private int curChar;
+	private int numChars;
+	private SpaceCharFilter filter;
+
+	public InputReader(InputStream stream) {
+		this.stream = stream;
+	}
+
+	public int read() {
+		if (numChars == -1)
+			throw new InputMismatchException();
+		if (curChar >= numChars) {
+			curChar = 0;
+			try {
+				numChars = stream.read(buf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (numChars <= 0)
+				return -1;
+		}
+		return buf[curChar++];
+	}
+
+	public int readInt() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		int sgn = 1;
+		if (c == '-') {
+			sgn = -1;
+			c = read();
+		}
+		int res = 0;
+		do {
+			if (c < '0' || c > '9')
+				throw new InputMismatchException();
+			res *= 10;
+			res += c - '0';
+			c = read();
+		} while (!isSpaceChar(c));
+		return res * sgn;
+	}
+
+	public String readLine() {
+		int c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (c != '\n');
+		return res.toString();
+	}
+	
+	public String readString() {
+		int c = read();
+		while (isSpaceChar(c))
+			c = read();
+		StringBuilder res = new StringBuilder();
+		do {
+			res.appendCodePoint(c);
+			c = read();
+		} while (!isSpaceChar(c));
+		return res.toString();
+	}
+
+	public boolean isSpaceChar(int c) {
+		if (filter != null)
+			return filter.isSpaceChar(c);
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	}
+
+	public String next() {
+		return readString();
+	}
+
+	public interface SpaceCharFilter {
+		public boolean isSpaceChar(int ch);
+	}
+}
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+
+	public void flush() {
+		writer.flush();
+	}
+
+}
+
+class IOUtils {
+
+	public static int[] readIntArray(InputReader in, int size) {
+		int[] array = new int[size];
+		for (int i = 0; i < size; i++)
+			array[i] = in.readInt();
+		return array;
+	}
+
+}
+
+
