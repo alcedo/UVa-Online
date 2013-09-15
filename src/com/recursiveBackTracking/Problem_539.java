c8d07bed69549bc800011aa156ab06214180a910
victor <konsolidate@gmail.com>
2013-09-15 21:02:50 +0800

    finished 539

---
+++ src/com/recursiveBackTracking/Problem_539.java
@@ -0,0 +1,294 @@
+package com.recursiveBackTracking;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+import static java.lang.System.out;
+
+/**
+ * This is like a brute force depth first search traversal sln... 
+ */
+public class Problem_539 {
+	public static final boolean DEBUG = false;
+	
+    public static void main(String[] args) throws IOException {
+    	 
+		BufferedOutputStream output = new BufferedOutputStream(System.out);
+		InputReader input           = new InputReader(System.in);
+		StringBuilder sb = new StringBuilder(2000); //assume initial size of 2000 chars
+		  
+		int nodes;  
+		while((nodes = input.readInt()) != 0) {
+			
+			int edges = input.readInt(); 
+			int[] from = new int[edges];
+			int[] to = new int[edges];
+			
+			for(int i=0; i<edges; i++) {
+				from[i] = input.readInt();
+				to[i] = input.readInt();
+			}
+			
+			
+			int max=0;
+			for(int i=0; i<edges; i++) //this loop is to try out each node as our Search Starting point
+				max = Math.max(max, maxLength(new HashSet<Integer>(), i, from, to, new ArrayList<Integer>(), new ArrayList<Integer>()));
+
+			
+			sb.append(max);
+			sb.append("\n");
+		}
+		
+		output.write(sb.toString().getBytes());
+		output.flush();
+		output.close();
+    
+    }// end of static void main
+    
+    public static int maxLength(HashSet<Integer> edgeSet, int nodeId, int[] from, int[] to, 
+    		ArrayList<Integer> cFrom, ArrayList<Integer> cTo){
+    
+    	int maxLength = 0;
+    	for(int i=0; i<from.length; i++) {
+
+    		if(edgeSet.contains(i)) //ignore edges that has been visited before 
+    			continue;
+    		
+    		if(from[i] == nodeId) {
+    			
+    			edgeSet.add(i);
+    			cFrom.add(from[i]);
+    			cTo.add(to[i]);
+    		
+    			maxLength = Math.max(maxLength, maxLength(edgeSet, to[i], from, to, cFrom, cTo  ) +1 );
+    			
+    			edgeSet.remove(i);
+    			cFrom.remove(cFrom.size()-1);
+    			cTo.remove(cTo.size()-1);
+    		}
+    	}
+    	
+    	// try the other way too! as long as edges hasn't been traversed before 
+    	// we are trying the other way because graph is un-directed.
+    	for(int i=0; i<to.length; i++) {
+
+    		if(edgeSet.contains(i)) //ignore edges that has been visited before 
+    			continue;
+    		
+
+    		if(to[i] == nodeId) {
+    			
+    			edgeSet.add(i);
+    			cFrom.add(to[i]);
+    			cTo.add(from[i]);
+    		
+    			maxLength = Math.max(maxLength, maxLength(edgeSet, from[i], from, to, cFrom, cTo  ) +1 );
+    			
+    			edgeSet.remove(i);
+    			cFrom.remove(cFrom.size()-1);
+    			cTo.remove(cTo.size()-1);
+    		}
+    	}
+    	
+// unComment to see traversal path    	
+//    	for(int i=0; i<cFrom.size(); i++){ 
+//    		out.print(" " + cFrom.get(i) + " -> " + cTo.get(i) + " ");
+//    	}out.println("");
+    	
+    	return maxLength;
+    }
+    
+    
+    
+    static class InputReader {
+
+    	private InputStream stream;
+    	private byte[] buf = new byte[1024];
+    	private int curChar;
+    	private int numChars;
+    	private SpaceCharFilter filter;
+
+    	public InputReader(InputStream stream) {
+    		this.stream = stream;
+    	}
+
+    	public int read() {
+    		if (numChars == -1)
+    			throw new InputMismatchException();
+    		if (curChar >= numChars) {
+    			curChar = 0;
+    			try {
+    				numChars = stream.read(buf);
+    			} catch (IOException e) {
+    				throw new InputMismatchException();
+    			}
+    			if (numChars <= 0)
+    				return -1;
+    		}
+    		return buf[curChar++];
+    	}
+
+    	public int readInt() {
+    		int c = read();
+    		while (isSpaceChar(c))
+    			c = read();
+    		int sgn = 1;
+    		if (c == '-') {
+    			sgn = -1;
+    			c = read();
+    		}
+    		int res = 0;
+    		do {
+    			if (c < '0' || c > '9')
+    				throw new InputMismatchException();
+    			res *= 10;
+    			res += c - '0';
+    			c = read();
+    		} while (!isSpaceChar(c));
+    		return res * sgn;
+    	}
+
+    	public String readLine() {
+    		int c = read();
+    		//theres no ASCII -1 value. this signify EOF, and we return empty string
+    		if(c == -1) return ""; 
+    		
+    		StringBuilder res = new StringBuilder();
+    		do {
+    			res.appendCodePoint(c);
+    			c = read();
+    		} while (c != '\n' );
+    		return res.toString();
+    	}
+    	
+    	public String readString() {
+    		int c = read();
+    		while (isSpaceChar(c))
+    			c = read();
+    		StringBuilder res = new StringBuilder();
+    		do {
+    			res.appendCodePoint(c);
+    			c = read();
+    		} while (!isSpaceChar(c));
+    		return res.toString();
+    	}
+
+    	public boolean isSpaceChar(int c) {
+    		if (filter != null)
+    			return filter.isSpaceChar(c);
+    		return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    	}
+
+    	public String next() {
+    		return readString();
+    	}
+
+    	public interface SpaceCharFilter {
+    		public boolean isSpaceChar(int ch);
+    	}
+    }
+
+    static class OutputWriter {
+    	private final PrintWriter writer;
+
+    	public OutputWriter(OutputStream outputStream) {
+    		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    	}
+
+    	public OutputWriter(Writer writer) {
+    		this.writer = new PrintWriter(writer);
+    	}
+
+    	public void print(Object...objects) {
+    		for (int i = 0; i < objects.length; i++) {
+    			if (i != 0)
+    				writer.print(' ');
+    			writer.print(objects[i]);
+    		}
+    	}
+
+    	public void printLine(Object...objects) {
+    		print(objects);
+    		writer.println();
+    	}
+
+    	public void close() {
+    		writer.close();
+    	}
+
+    	public void flush() {
+    		writer.flush();
+    	}
+    }
+
+    static class IOUtils {
+
+    	public static int[] readIntArray(InputReader in, int size) {
+    		int[] array = new int[size];
+    		for (int i = 0; i < size; i++)
+    			array[i] = in.readInt();
+    		return array;
+    	}
+    }
+
+    
+    
+	    
+}//End of Main Class
+
+
+/**************************************************************************
+ *  Fast I/O functions (prevents i/o flushing by buffering)
+ **************************************************************************/
+//BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+/* 
+Slightly less efficient 
+BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out)); 
+output.write(stringBuilder.toString());
+
+More efficient: 
+BufferedOutputStream output = new BufferedOutputStream(System.out); // more efficient  
+output.write(stringBuilder.toString().getBytes());
+*/
+
+//or we can use the below utilitiy classes: 
+
+/** 
+* Various utility class for Fast I/O in java.  
+* Readings: http://www.codechef.com/wiki/java#IO_in_Java 
+* BufferedInputStream > BufferedReader > Scanner  
+* 
+* USAGE: 
+* InputReader in 		= new InputReader(System.in);
+* OutputWriter out	=	new OutputWriter(System.out);
+* 
+* int i = in.readInt(); //read int
+* String s = in.readString(); //read string
+* int[] x = IOUtils.readIntArray(in,N); //read int array of size N
+* String s = in.readLine() // read a line ( to be used with tokenizer );
+* 
+* out.printLine("X");
+*  
+* out.flush(); // flush output
+* out.close(); // remember to close the outputstream, at the end (might be able to not do this to save some time) 
+*/
+
+
+
+
+
